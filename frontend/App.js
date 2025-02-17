3f29b369a94a6c1e4d052a972f2b8f496cd3836d
Nemsara Ranaba <ewnemsara123@gmail.com>
2025-02-17 21:47:59 +0530

    Revert "Leader board"

--- frontend/App.js
+++ frontend/App.js
@@ -1,34 +1,30 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LeaderBoardScreen from './screens/LeaderBoard/LeaderBoardScreen';
-import LeaderBoardItem from './screens/LeaderBoard/LeaderBoardItem';
-
-import { StyleSheet } from "react-native";
+import MenuScreen from './screens/MenuScreen';
+import SignToTextScreen from './screens/SignToText/SignToTextScreen';
+import TextToSignScreen from './screens/TextToSign/TextToSignScreen';
+import SignLanguageDictionaryScreen from './screens/SignLanguageDictionary/SignLanguageDictionaryScreen';
+import SchoolScreen from './screens/SignLanguageSchool/SignLanguageSchoolScreen';
+import CommunityForumScreen from './screens/CommunityForum/CommunityForumScreen';
+import SignLanguagePhraseBookScreen from './screens/SignLanguagePraseBook/SignLanguagePhraseBookScreen';
 
 const Stack = createStackNavigator();
 
 const App = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name='LeaderBoardScreen' component={LeaderBoardScreen}/>
-                <Stack.Screen name='LeaderBoardItem' component={LeaderBoardItem}/>
-                {/* Add other screens here */}
+            <Stack.Navigator initialRouteName="Menu">
+                <Stack.Screen name="Menu" component={MenuScreen} />
+                <Stack.Screen name="SignToText" component={SignToTextScreen} />
+                <Stack.Screen name="TextToSign" component={TextToSignScreen} />
+                <Stack.Screen name="Dictionary" component={SignLanguageDictionaryScreen} />
+                <Stack.Screen name="Phrasebook" component={SignLanguagePhraseBookScreen} />
+                <Stack.Screen name="School" component={SchoolScreen} />
+                <Stack.Screen name="CommunityForum" component={CommunityForumScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
 };
 
 export default App;
-
-
-export const sharedStyles = StyleSheet.create({
-  shadow: {
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 3,
-  },
-});
