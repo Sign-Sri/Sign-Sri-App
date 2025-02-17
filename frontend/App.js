fa5482c216d2602d5d01df5abc7cc45a3863360a
Nemsara Ranaba <ewnemsara123@gmail.com>
2025-02-18 01:46:18 +0530

    Updated the app.js for the Merging part

--- frontend/App.js
+++ frontend/App.js
@@ -1,34 +1,11 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import MenuScreen from './screens/MenuScreen';
-import SignToTextScreen from './screens/SignToText/SignToTextScreen';
-import TextToSignScreen from './screens/TextToSign/TextToSignScreen';
-import SignLanguageDictionaryScreen from './screens/SignLanguageDictionary/SignLanguageDictionaryScreen';
-import SchoolScreen from './screens/SignLanguageSchool/SignLanguageSchoolScreen';
-import CommunityForumScreen from './screens/CommunityForum/CommunityForumScreen';
-import SignLanguagePhraseBookScreen from './screens/SignLanguagePraseBook/SignLanguagePhraseBookScreen';
-import PageNavigation from './screens/CommunityForum/Navigation/PageNavigation';
-import SortByScreen from './screens/CommunityForum/SortByScreen';
+import StackNavigator from './screens/Navigation/StackNavigator';
 
-
-const Stack = createStackNavigator();
 
 const App = () => {
     return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Menu">
-                <Stack.Screen name="Menu" component={MenuScreen} />
-                <Stack.Screen name="SignToText" component={SignToTextScreen} />
-                <Stack.Screen name="TextToSign" component={TextToSignScreen} />
-                <Stack.Screen name="Dictionary" component={SignLanguageDictionaryScreen} />
-                <Stack.Screen name="Phrasebook" component={SignLanguagePhraseBookScreen} />
-                <Stack.Screen name="School" component={SchoolScreen} />
-                <Stack.Screen name="CommunityForum" component={CommunityForumScreen} />
-                <Stack.Screen name="PageNavigation" component={PageNavigation} />
-                <Stack.Screen name="SortBy" component={SortByScreen}/>
-            </Stack.Navigator>
-        </NavigationContainer>
+            <StackNavigator />
+
     );
 };
 
