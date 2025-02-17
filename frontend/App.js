98861d26e48605c251f6d77bcf204a3faa4b2a5f
Nemsara Ranaba <ewnemsara123@gmail.com>
2025-02-17 09:51:22 +0530

    .

--- frontend/App.js
+++ frontend/App.js
@@ -1,36 +1,15 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import MenuScreen from './screens/MenuScreen';
-import SignToTextScreen from './screens/SignToText/SignToTextScreen';
-import TextToSignScreen from './screens/TextToSign/TextToSignScreen';
-import SignLanguageDictionaryScreen from './screens/SignLanguageDictionary/SignLanguageDictionaryScreen';
-import SignLanguageSchoolScreen from './screens/SignLanguageSchool/SignLanguageSchoolScreen';
-import CommunityForumScreen from './screens/CommunityForum/CommunityForumScreen';
-import SignLanguagePhraseBookScreen from './screens/SignLanguagePraseBook/SignLanguagePhraseBookScreen';
-import LessonScreen from './screens/SignLanguageSchool/LessonScreen';
-import QuizScreen from './screens/SignLanguageSchool/QuizScreen';
-import ProgressScreen from './screens/SignLanguageSchool/ProgressScreen';
+import StackNavigator from './screens/Navigation/StackNavigator';
 
-const Stack = createStackNavigator();
+
+
+
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Menu">
-        <Stack.Screen name="Menu" component={MenuScreen} />
-        <Stack.Screen name="SignToText" component={SignToTextScreen} />
-        <Stack.Screen name="TextToSign" component={TextToSignScreen} />
-        <Stack.Screen name="Dictionary" component={SignLanguageDictionaryScreen} />
-        <Stack.Screen name="Phrasebook" component={SignLanguagePhraseBookScreen} />
-        <Stack.Screen name="School" component={SignLanguageSchoolScreen} />
-        <Stack.Screen name="CommunityForum" component={CommunityForumScreen} />
-        <Stack.Screen name="Lesson" component={LessonScreen} />
-        <Stack.Screen name="Quiz" component={QuizScreen} />
-        <Stack.Screen name="Progress" component={ProgressScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+            <StackNavigator />
+
+    );
 };
 
 export default App;
