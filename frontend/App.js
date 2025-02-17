19426feb2a105d1ad4a53762b0dfcd51485ff789
Nemsara Ranaba <ewnemsara123@gmail.com>
2025-02-17 21:40:47 +0530

    updated app.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,34 +1,13 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import LeaderBoardScreen from './screens/LeaderBoard/LeaderBoardScreen';
-import LeaderBoardItem from './screens/LeaderBoard/LeaderBoardItem';
+import StackNavigator from './screens/Navigation/StackNavigator';
 
-import { StyleSheet } from "react-native";
-
-const Stack = createStackNavigator();
 
 const App = () => {
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name='LeaderBoardScreen' component={LeaderBoardScreen}/>
-                <Stack.Screen name='LeaderBoardItem' component={LeaderBoardItem}/>
-                {/* Add other screens here */}
-            </Stack.Navigator>
-        </NavigationContainer>
+            <StackNavigator />
+
     );
 };
 
 export default App;
 
-
-export const sharedStyles = StyleSheet.create({
-  shadow: {
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 3,
-  },
-});
