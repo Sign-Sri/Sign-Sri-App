38984c6645ca17c7634d4fe8c62b604cc58af478
Nemsara Ranaba <ewnemsara123@gmail.com>
2025-02-20 09:26:26 +0530

    hhv

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 // Routes
 app.use("/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
