66ae71426956b1aa46af85d59d1e2178d3b3aa1c
Pyara Perera <145968773+PyaraKalashi@users.noreply.github.com>
2025-03-10 13:06:50 +0530

    Update post.routes.js

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const admin = require("../config/firebase-config");
+
 const verifyToken = require('../middleware/verifyToken');
 const Post = require('../models/post.model');
 
