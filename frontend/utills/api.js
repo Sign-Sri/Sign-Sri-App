41ef8ee306d1bc9fba22f5729f8e4a12718f7a22
Pyara Perera <145968773+PyaraKalashi@users.noreply.github.com>
2025-03-18 22:31:11 +0530

    Create api.js

---
+++ frontend/utills/api.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { API_URL } from '../../config/firebaseConfig';
+
+const api = axios.create({
+    baseURL: API_URL,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  
+  // Add a request interceptor to include the auth token in requests
+api.interceptors.request.use(
+    async (config) => {
+      try {
+        const token = await AsyncStorage.getItem('authToken');
+        if (token) {
+          config.headers.Authorization = `Bearer ${token}`;
+        }
+      } catch (error) {
+        console.error('Error getting auth token:', error);
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+  
+  export default api;
