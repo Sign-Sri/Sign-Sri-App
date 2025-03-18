e3a7d8a44f00494ccbe969ca53aa0265128dc4db
Pyara Perera <145968773+PyaraKalashi@users.noreply.github.com>
2025-03-18 14:21:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const authRoutes = require('./routes/auth.routes');
 const admin = require('./config/firebase-config');
+const communityRoutes = require("./routes/communitypage")
 
 const app = express();
 
