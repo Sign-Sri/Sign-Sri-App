be6d0ecf41280af2a449e75f0d4c1bdd5d10065c
Pyara Perera <145968773+PyaraKalashi@users.noreply.github.com>
2025-03-19 19:20:20 +0530

    Update forumRoutes.js

--- backend/routes/forumRoutes.js
+++ backend/routes/forumRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const db = require("./../config/firebase-Config");
 const router = express.Router();
 
-/ Create a new post
+// Create a new post
 router.post("/posts", async (req, res) => {
   try {
     const { userId, content, imageUrl } = req.body;
@@ -20,3 +20,14 @@ router.post("/posts", async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+
+// Get all posts
+router.get("/posts", async (req, res) => {
+    try {
+      const postsSnapshot = await db.collection("posts").orderBy("timestamp", "desc").get();
+      const posts = postsSnapshot.docs.map(doc => ({ postId: doc.id, ...doc.data() }));
+      res.status(200).json(posts);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
