afe6f7933348d1943a92b4a74b26ac30b1b4056d
Nemsara Ranaba <ewnemsara123@gmail.com>
2025-03-31 14:56:17 +0530

    updated

--- frontend/screens/TextToSign/TextToSignScreen.js
+++ frontend/screens/TextToSign/TextToSignScreen.js
@@ -36,7 +36,7 @@ const TextToSignScreen = () => {
     try {
       // Send a POST request to the backend to generate the ASL GIF
       console.log('Sending request to backend');
-      const response = await fetch('http://192.168.8.177:3000/convert', {
+      const response = await fetch('http://10.31.11.145:3000/convert', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -60,7 +60,7 @@ const TextToSignScreen = () => {
       }
 
       // Construct the URL for the generated GIF and update state
-      const gifUrl = `http://192.168.8.177:3000${data.name}`;
+      const gifUrl = `http://10.31.11.145:3000${data.name}`;
       console.log('GIF URL:', gifUrl);
 
       setAslGif({ uri: gifUrl });
